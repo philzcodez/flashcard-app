5f6027cfc85610d3d5d52f90800fd4e5d89212fe
philz-codez <memetic.trivia1t@icloud.com>
2026-02-08 16:41:56 -0500

    Initial Commit

---
+++ GoStudy/GoStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoStudyApp.swift
+//  GoStudy
+//
+//  Created by Phillip Bilenko on 2/8/26.
+//
+
+import SwiftUI
+
+@main
+struct GoStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
